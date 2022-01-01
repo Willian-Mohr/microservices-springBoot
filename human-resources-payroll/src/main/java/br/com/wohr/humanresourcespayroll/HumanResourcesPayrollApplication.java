f484951799fe28afdd6e7033e65b39ddfafcedb3
Willian Mohr <willianbmohr@gmail.com>
2022-01-01 20:48:05 -0300

    Feign client

--- human-resources-payroll/src/main/java/br/com/wohr/humanresourcespayroll/HumanResourcesPayrollApplication.java
+++ human-resources-payroll/src/main/java/br/com/wohr/humanresourcespayroll/HumanResourcesPayrollApplication.java
@@ -2,10 +2,12 @@ package br.com.wohr.humanresourcespayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
+@RibbonClient(name = "human-resources-worker")
 public class HumanResourcesPayrollApplication {
 
 	public static void main(String[] args) {
--- human-resources-worker/src/main/java/br/com/wohr/humanresourcesworker/resources/WorkerResource.java
+++ human-resources-worker/src/main/java/br/com/wohr/humanresourcesworker/resources/WorkerResource.java
@@ -3,8 +3,10 @@ package br.com.wohr.humanresourcesworker.resources;
 import java.util.List;
 import java.util.Optional;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,10 +18,15 @@ import br.com.wohr.humanresourcesworker.repositories.WorkerRepository;
 
 @Controller
 @RequestMapping(value = "/workers")
-public class workerResource {
+public class WorkerResource {
 
 	@Autowired
 	private WorkerRepository repository;
+	
+	@Autowired
+	private Environment environment;
+	
+	private static Logger logger = LoggerFactory.getLogger(WorkerResource.class);
 
 	@GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
@@ -31,6 +38,8 @@ public class workerResource {
 	
 	@GetMapping(value = "/{id}")
 	public ResponseEntity<Worker> findById(@PathVariable Long id) throws Exception {
+		
+		logger.info("PORT = " + environment.getProperty("local.server.port"));
 
 		Optional<Worker> worker = repository.findById(id);
 
