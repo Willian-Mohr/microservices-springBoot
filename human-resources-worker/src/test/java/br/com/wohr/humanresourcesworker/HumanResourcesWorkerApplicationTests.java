cd3bbbd3741b14f645f7ae3220945af4d3c2f995
Willian Mohr <willianbmohr@gmail.com>
2022-01-01 16:53:32 -0300

    Project hr-worker created

---
+++ human-resources-worker/src/test/java/br/com/wohr/humanresourcesworker/HumanResourcesWorkerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesworker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesWorkerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
