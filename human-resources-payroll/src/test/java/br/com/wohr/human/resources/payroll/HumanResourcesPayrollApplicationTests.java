d293779b71bb677ee220157e3e046d5dc6b209fd
Willian Mohr <willianbmohr@gmail.com>
2022-01-01 18:02:56 -0300

    Project hr-payroll created

---
+++ human-resources-payroll/src/test/java/br/com/wohr/human/resources/payroll/HumanResourcesPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.human.resources.payroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesPayrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
