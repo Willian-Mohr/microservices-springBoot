c6ff12ab6afe468b1148a968e9a144c1db63b70d
Willian Mohr <willianbmohr@gmail.com>
2022-01-02 16:51:41 -0300

    Configuring Eureka clients
    - Remoção do Ribbon Client, pois o Eureka já possui o balanceamento de carga.

--- human-resources-payroll/src/main/java/br/com/wohr/humanresourcespayroll/HumanResourcesPayrollApplication.java
+++ human-resources-payroll/src/main/java/br/com/wohr/humanresourcespayroll/HumanResourcesPayrollApplication.java
@@ -2,12 +2,12 @@ package br.com.wohr.humanresourcespayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.ribbon.RibbonClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@RibbonClient(name = "human-resources-worker")
+@EnableEurekaClient
 public class HumanResourcesPayrollApplication {
 
 	public static void main(String[] args) {
