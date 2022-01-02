6f28bc9839020329de2bd94ec39a196d44f2bfeb
Willian Mohr <willianbmohr@gmail.com>
2022-01-02 13:22:41 -0300

    Project human-resources-eureka-server created

---
+++ human-resources-eureka-server/src/test/java/br/com/wohr/humanresourceseurekaserver/HumanResourcesEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourceseurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
