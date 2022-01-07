7c36d60ee40172d6682579c045a85492725a2120
Willian Mohr <willianbmohr@gmail.com>
2022-01-07 00:14:11 -0300

    Private Git repository
    - Configurando acesso ao repositório privado

--- human-resources-config-server/src/main/java/br/com/wohr/humanresourcesconfigserver/HumanResourcesConfigServerApplication.java
+++ human-resources-config-server/src/main/java/br/com/wohr/humanresourcesconfigserver/HumanResourcesConfigServerApplication.java
@@ -1,15 +1,27 @@
 package br.com.wohr.humanresourcesconfigserver;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class HumanResourcesConfigServerApplication {
+public class HumanResourcesConfigServerApplication implements CommandLineRunner {
+
+	@Value("${spring.cloud.config.server.git.password}")
+	private String username;
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourcesConfigServerApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		System.out.println("USERNAME: " + username);
+
 	}
 
 }
