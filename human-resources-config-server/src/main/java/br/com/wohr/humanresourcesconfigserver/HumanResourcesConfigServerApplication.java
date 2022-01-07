319df236405864b29aac2804dcba9dd749f04512
Willian Mohr <willianbmohr@gmail.com>
2022-01-06 23:05:02 -0300

    Project human-resources-config-server created

---
+++ human-resources-config-server/src/main/java/br/com/wohr/humanresourcesconfigserver/HumanResourcesConfigServerApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesconfigserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesConfigServerApplication.class, args);
+	}
+
+}
---
+++ human-resources-config-server/src/test/java/br/com/wohr/humanresourcesconfigserver/HumanResourcesConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
