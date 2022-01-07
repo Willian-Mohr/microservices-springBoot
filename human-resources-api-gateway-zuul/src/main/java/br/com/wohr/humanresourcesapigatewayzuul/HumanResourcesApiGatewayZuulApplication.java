841d23319f6c82351d2ea8cb21fb26c93937bbdb
Willian Mohr <willianbmohr@gmail.com>
2022-01-06 22:27:31 -0300

    Project human-resources-api-gateway-zuul created

---
+++ human-resources-api-gateway-zuul/src/main/java/br/com/wohr/humanresourcesapigatewayzuul/HumanResourcesApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesapigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesApiGatewayZuulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesApiGatewayZuulApplication.class, args);
+	}
+
+}
---
+++ human-resources-api-gateway-zuul/src/test/java/br/com/wohr/humanresourcesapigatewayzuul/HumanResourcesApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
