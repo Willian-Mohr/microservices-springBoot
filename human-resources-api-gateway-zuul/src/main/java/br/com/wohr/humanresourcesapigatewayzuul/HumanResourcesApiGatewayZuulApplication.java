9b05af5c62c574402c04c347bb41f58e5e722bc0
Willian Mohr <willianbmohr@gmail.com>
2022-01-06 22:49:22 -0300

    Configuring Zuul api gateway
    - API Gateway Zuul é um projeto que executa o roteamento dinâmico de aplicativos de microsserviços.
    - Gerado nova versão da collection

--- human-resources-api-gateway-zuul/src/main/java/br/com/wohr/humanresourcesapigatewayzuul/HumanResourcesApiGatewayZuulApplication.java
+++ human-resources-api-gateway-zuul/src/main/java/br/com/wohr/humanresourcesapigatewayzuul/HumanResourcesApiGatewayZuulApplication.java
@@ -2,8 +2,12 @@ package br.com.wohr.humanresourcesapigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @SpringBootApplication
+@EnableZuulProxy
+@EnableEurekaClient
 public class HumanResourcesApiGatewayZuulApplication {
 
 	public static void main(String[] args) {
