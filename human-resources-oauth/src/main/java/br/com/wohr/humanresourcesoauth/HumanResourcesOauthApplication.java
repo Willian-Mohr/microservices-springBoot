c3fd9ec64d0b33cafb600717fdea77bed251b77f
Willian Mohr <willianbmohr@gmail.com>
2022-01-09 14:57:25 -0300

    Project human-resources-oauth created

---
+++ human-resources-oauth/src/main/java/br/com/wohr/humanresourcesoauth/HumanResourcesOauthApplication.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HumanResourcesOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HumanResourcesOauthApplication.class, args);
+	}
+
+}
---
+++ human-resources-oauth/src/test/java/br/com/wohr/humanresourcesoauth/HumanResourcesOauthApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wohr.humanresourcesoauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourcesOauthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
