4b1cb5b190a78f20f7a8b23c9cf18b1b360339e9
Willian Mohr <willianbmohr@gmail.com>
2022-01-09 13:44:24 -0300

    Configuring human-resources-user project

--- human-resources-user/src/main/java/br/com/wohr/humanresourcesuser/HumanResourcesUserApplication.java
+++ human-resources-user/src/main/java/br/com/wohr/humanresourcesuser/HumanResourcesUserApplication.java
@@ -2,8 +2,10 @@ package br.com.wohr.humanresourcesuser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class HumanResourcesUserApplication {
 
 	public static void main(String[] args) {
