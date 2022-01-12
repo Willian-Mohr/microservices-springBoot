1f18a992e84297ddc260a7432e0caa9a89ce8193
Willian Mohr <willianbmohr@gmail.com>
2022-01-12 11:57:56 -0300

    Login, JWT token
    - Dependência para jaxb-impl foi adiciona, pois, estava estourando nullPointerException ao instanciar o JwtSecurity

--- human-resources-oauth/src/main/java/br/com/wohr/humanresourcesoauth/services/UserService.java
+++ human-resources-oauth/src/main/java/br/com/wohr/humanresourcesoauth/services/UserService.java
@@ -3,13 +3,16 @@ package br.com.wohr.humanresourcesoauth.services;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import br.com.wohr.humanresourcesoauth.entities.User;
 import br.com.wohr.humanresourcesoauth.feignclients.UserFeignClient;
 
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserFeignClient userFeignClient;
@@ -28,8 +31,23 @@ public class UserService {
 		}
 
 		logger.info("Email found: " + email);
+		
 		return user;
 
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		User user = userFeignClient.findByIdEmail(username).getBody();
+
+		if (user == null) {
+
+			throw new UsernameNotFoundException("Email not found");
+
+		}
+		
+		return user;
 	}
 
 }
