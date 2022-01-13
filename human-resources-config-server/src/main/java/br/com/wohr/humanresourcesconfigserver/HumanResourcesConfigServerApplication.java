130e1ade1f7da2edbf2804868d78c4cfa7ce370c
Willian Mohr <willianbmohr@gmail.com>
2022-01-12 23:47:00 -0300

    Removing CommandLineRunner and adjusting the .properties

--- human-resources-config-server/src/main/java/br/com/wohr/humanresourcesconfigserver/HumanResourcesConfigServerApplication.java
+++ human-resources-config-server/src/main/java/br/com/wohr/humanresourcesconfigserver/HumanResourcesConfigServerApplication.java
@@ -1,27 +1,15 @@
 package br.com.wohr.humanresourcesconfigserver;
 
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
 @EnableConfigServer
-public class HumanResourcesConfigServerApplication implements CommandLineRunner {
-
-	@Value("${spring.cloud.config.server.git.password}")
-	private String username;
+public class HumanResourcesConfigServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HumanResourcesConfigServerApplication.class, args);
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		System.out.println("USERNAME: " + username);
-
 	}
 
 }
